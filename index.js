e3d426ac249cf3be243a6143ef2f8225f1469561
DiegoJ07 <dieguitojunin07@gmail.com>
2024-06-11 17:36:25 -0300

    Finalizacion de Proyecto Base.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const items = document.querySelectorAll('.item');
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".item");
 
-    function checkPosition() {
-        const windowHeight = window.innerHeight;
-        items.forEach(item => {
-            const positionFromTop = item.getBoundingClientRect().top;
+  function checkPosition() {
+    const windowHeight = window.innerHeight;
+    items.forEach((item) => {
+      const positionFromTop = item.getBoundingClientRect().top;
 
-            if (positionFromTop - windowHeight <= -100) {
-                item.classList.add('show');
-                item.classList.remove('hidden');
-            }
-        });
-    }
+      if (positionFromTop - windowHeight <= -100) {
+        item.classList.add("show");
+        item.classList.remove("hidden");
+      }
+    });
+  }
 
-    window.addEventListener('scroll', checkPosition);
-    checkPosition();
+  window.addEventListener("scroll", checkPosition);
+  checkPosition();
 });
