eeae805e7214c0dc8f6894b19f82270a4a0f4b37
Diego <dieguitojunin07@gmail.com>
2024-06-18 21:40:07 -0300

    Finalizacion de la Web.

---
+++ front/scripts/index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".item");
+
+  function checkPosition() {
+    const windowHeight = window.innerHeight;
+    items.forEach((item) => {
+      const positionFromTop = item.getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= -100) {
+        item.classList.add("show");
+        item.classList.remove("hidden");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", checkPosition);
+  checkPosition();
+});
